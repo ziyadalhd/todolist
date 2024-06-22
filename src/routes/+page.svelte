3eeca772cefc2941d246ba35c3c4cdf2eceed23e
ziyad <ziyadalhdriti@gmail.com>
2024-06-23 02:50:32 +0300

    Header compount

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,7 @@
 <script lang="ts">
-  import dayjs from "dayjs";
-  import "dayjs/locale/ar";
-
-  dayjs.locale("ar");
-
-  let time = dayjs().format("A") == "ص" ? "صباح" : "مساء";
+  import Header from "$lib/Header.svelte";
 </script>
 
 <div class="p-16">
-  <header class="flex justify-between">
-    <div>
-      <h1 class="text-3xl mb-2">{time} الخير👋</h1>
-      <h2 class="text-xl text-surface-900/50">
-        {dayjs().format("اليوم, dddd D MMMM YYYY")}
-      </h2>
-    </div>
-    <div class="bg-white w-32 h-12 rounded-xl"></div>
-  </header>
+  <Header />
 </div>
