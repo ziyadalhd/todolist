e69cfcc084bf84478e195e9503bf2fbb7e64add1
ziyad <ziyadalhdriti@gmail.com>
2024-06-23 02:42:29 +0300

    التوقيت

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,20 @@
+<script lang="ts">
+  import dayjs from "dayjs";
+  import "dayjs/locale/ar";
+
+  dayjs.locale("ar");
+
+  let time = dayjs().format("A") == "ص" ? "صباح" : "مساء";
+</script>
+
 <div class="p-16">
   <header class="flex justify-between">
     <div>
-      <h1 class="text-3xl mb-2">صباح الخير👋</h1>
-      <h2 class="text-xl text-surface-900/50">اليوم, الاحد</h2>
+      <h1 class="text-3xl mb-2">{time} الخير👋</h1>
+      <h2 class="text-xl text-surface-900/50">
+        {dayjs().format("اليوم, dddd D MMMM YYYY")}
+      </h2>
     </div>
-    <div class="bg-red-500 w-32 h-32"></div>
+    <div class="bg-white w-32 h-12 rounded-xl"></div>
   </header>
 </div>
