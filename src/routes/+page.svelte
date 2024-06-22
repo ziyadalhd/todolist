d96349f4cb98b0d95a428fc01c416f40b0163a29
ziyad <ziyadalhdriti@gmail.com>
2024-06-23 02:14:13 +0300

    the page head

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,9 @@
-<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
-
-<div class="container h-full mx-auto flex justify-center items-center">
-	<div class="space-y-5">
-		<h1 class="h1">Let's get cracking bones!</h1>
-		<p>Start by exploring:</p>
-		<ul>
-			<li><code class="code">/src/routes/+layout.svelte</code> - barebones layout</li>
-			<li><code class="code">/src/app.postcss</code> - app wide css</li>
-			<li>
-				<code class="code">/src/routes/+page.svelte</code> - this page, you can replace the contents
-			</li>
-		</ul>
-	</div>
+<div class="p-16">
+  <header class="flex justify-between">
+    <div>
+      <h1 class="text-3xl mb-2">صباح الخير👋</h1>
+      <h2 class="text-xl text-surface-900/50">اليوم, الاحد</h2>
+    </div>
+    <div class="bg-red-500 w-32 h-32"></div>
+  </header>
 </div>
